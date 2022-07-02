522fe66e4b741759062d2d4a5355c11e16712655
le anh trung <84488011+trungle2021@users.noreply.github.com>
2022-07-03 00:24:50 +0700

    OK

---
+++ TEACHER/TEACHER/Repository/ITeacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEACHER.Repository
+{
+    internal interface ITeacher
+    {
+    }
+}
