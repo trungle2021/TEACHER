57a6a903e585b3d8fcfdf43419a06857a1efaa82
Khang Nguyen <khangnqts2104024@fpt.edu.vn>
2022-07-04 20:25:15 +0700

    KHANG

--- TEACHER/TEACHER/Helper.cs
+++ TEACHER/TEACHER/Helper.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
--- TEACHER/TEACHER/Repository/ITeacher.cs
+++ TEACHER/TEACHER/Repository/ITeacher.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TEACHER.Model;
 
+
 namespace TEACHER.Repository
 {
     public interface ITeacher 
